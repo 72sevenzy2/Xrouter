857031879ff60f3273b1c649910d3c3a72e388eb
72 <72sevenzy2@gmail.com>
2026-05-28 18:27:03 +0500

    refactor: move test handler in internal/ directory (removed sub folders).

--- internal/hiHandler.go
+++ internal/hiHandler.go
@@ -1,16 +1,15 @@
-package handler
+package entity
 
 import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/72sevenzy2/http-router/internal/test-model"
 	"github.com/72sevenzy2/json-parser/helpers"
 )
 
 func HiHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var i entity.Entity // initilising the entity
+		var i Entity // initilising the entity
 
 		err := json.NewDecoder(r.Body).Decode(&i) // decoding the body to get the data we want
 		if err != nil {                           // if there is no data which we needed in the body, throw an json error msg

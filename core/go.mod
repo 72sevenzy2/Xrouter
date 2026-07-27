f667ad293611193b92661d1a18786abea4f966dc
72 <72sevenzy2@gmail.com>
2026-07-27 10:03:21 +0500

    refactor: added new module to hold shared types that will be from middlewares.go and router.go (will add later on).

---
+++ core/go.mod
@@ -0,0 +1,3 @@
+module xrouter/core
+
+go 1.25.5
---
+++ core/types.go
@@ -0,0 +1,5 @@
+package core
+
+// this package will hold shared types from the router and the middlewares, to avoid import cycles upon instillation.
+
+ 

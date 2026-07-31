3473da23572205304c7d8faa71bb5665b149839c
72 <72sevenzy2@gmail.com>
2026-07-31 18:15:26 +0500

    rename middleware.go to middleware_alias.go (will add function wrappers for middlewares later).

---
+++ router/middleware_alias.go
@@ -0,0 +1,16 @@
+package router
+
+import (
+	"github.com/72sevenzy2/http-router/core"
+)
+
+// todo: add middlewares as aliases from middleware/...
+
+// middleware chaining.
+func (r *Router) ApplyMiddlewares(h core.HandlerFunc) core.HandlerFunc {
+	for i := len(r.Middlewares) - 1; i >= 0; i-- {
+		h = r.Middlewares[i](h)
+	}
+
+	return h
+}

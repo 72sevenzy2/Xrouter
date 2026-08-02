269d8b9349593a2003db0a473166d0560214c1c3
72 <72sevenzy2@gmail.com>
2026-08-02 18:03:44 +0500

    feat: add middleware function wrappers for mws in middleware/.

--- router/middleware_alias.go
+++ router/middleware_alias.go
@@ -2,9 +2,10 @@ package router
 
 import (
 	"github.com/72sevenzy2/http-router/core"
+	mw "github.com/72sevenzy2/Xrouter-middlewares"
 )
 
-// todo: add middlewares as aliases from middleware/...
+// todo: add middlewares as variables wrapped from middleware/...
 
 // middleware chaining.
 func (r *Router) ApplyMiddlewares(h core.HandlerFunc) core.HandlerFunc {
@@ -13,4 +14,24 @@ func (r *Router) ApplyMiddlewares(h core.HandlerFunc) core.HandlerFunc {
 	}
 
 	return h
+}
+
+func Logger(conf uint32) core.Middleware {
+	return mw.Logger(conf)
+}
+
+func BasicAuth(user, pass string) core.Middleware {
+	return mw.BasicAuth(user, pass)
+}
+
+func BearerAuth(token string) core.Middleware {
+	return mw.BearerAuth(token)
+}
+
+func Recoverer() core.Middleware {
+	return mw.Recoverer()
+}
+
+func Timeout(N int) core.Middleware {
+	return mw.Timeout(N)
 }

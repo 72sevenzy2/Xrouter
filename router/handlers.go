daca1991dfb66b4700ddef4774943f2b7c2e861a
72 <72sevenzy2@gmail.com>
2026-08-11 19:02:55 +0500

    feat: add methods for Put, and Delete.

--- router/handlers.go
+++ router/handlers.go
@@ -9,6 +9,7 @@ import (
 
 // handler methods as alternative to Handle(...).
 
+// Router methods
 func (r *Router) Get(path string, handler core.HandlerFunc, mws ...core.Middleware) {
 	if len(mws) > 0 {
 		for v := range slices.Backward(mws) {
@@ -19,8 +20,8 @@ func (r *Router) Get(path string, handler core.HandlerFunc, mws ...core.Middlewa
 	if isDynamic(path) {
 		r.DynamicRoutes[http.MethodGet] = append(r.DynamicRoutes[http.MethodGet], core.Route{
 			Handler: handler,
-			Method: http.MethodGet,
-			Parts: splitPath(path),
+			Method:  http.MethodGet,
+			Parts:   splitPath(path),
 		})
 		return
 	}
@@ -42,14 +43,60 @@ func (r *Router) Post(path string, handler core.HandlerFunc, mws ...core.Middlew
 
 	if isDynamic(path) {
 		r.DynamicRoutes[http.MethodPost] = append(r.DynamicRoutes[http.MethodPost], core.Route{
-			Method: http.MethodPost,
+			Method:  http.MethodPost,
 			Handler: handler,
-			Parts: splitPath(path),
+			Parts:   splitPath(path),
 		})
+		return
 	}
-	
+
 	if r.StaticRoutes[path] == nil {
 		r.StaticRoutes[path] = make(map[string]core.HandlerFunc)
 	}
 	r.StaticRoutes[path][http.MethodPost] = handler
+}
+
+func (r *Router) Put(path string, handler core.HandlerFunc, mws ...core.Middleware) {
+	if len(mws) > 0 {
+		for v := range slices.Backward(mws) {
+			handler = mws[v](handler)
+		}
+	}
+
+	if isDynamic(path) {
+		r.DynamicRoutes[http.MethodPut] = append(r.DynamicRoutes[http.MethodPut], core.Route{
+			Method: http.MethodPut,
+			Handler: handler,
+			Parts: splitPath(path),
+		})
+		return
+	}
+
+	if r.StaticRoutes[path] == nil {
+		r.StaticRoutes = make(map[string]map[string]core.HandlerFunc)
+	}
+
+	r.StaticRoutes[path][http.MethodPut] = handler
+}
+
+func (r *Router) Del(path string, handler core.HandlerFunc, mws ...core.Middleware) {
+	if len(mws) > 0 {
+		for v := range slices.Backward(mws) {
+			handler = mws[v](handler)
+		}	
+	}
+
+	if isDynamic(path) {
+		r.DynamicRoutes[http.MethodDelete] = append(r.DynamicRoutes[http.MethodDelete], core.Route{
+			Handler: handler,
+			Method: http.MethodDelete,
+			Parts: splitPath(path),
+		})
+		return
+	}
+
+	if r.StaticRoutes[path] == nil {
+		r.StaticRoutes[path] = make(map[string]core.HandlerFunc)
+	}
+	r.StaticRoutes[path][http.MethodDelete] = handler
 }

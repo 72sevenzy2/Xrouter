0bd37a0a002d91b26d8c240ab3e6058f0da687af
72 <72sevenzy2@gmail.com>
2026-08-12 15:30:10 +0500

    feat: add additional http method handlers.

--- router/handlers.go
+++ router/handlers.go
@@ -24,3 +24,23 @@ func (r *Router) Put(path string, handler core.HandlerFunc, mws ...core.Middlewa
 func (r *Router) Del(path string, handler core.HandlerFunc, mws ...core.Middleware) {
 	r.Handle(http.MethodDelete, path, handler, mws...)
 }
+// connect, head, options, patch, tree
+func (r *Router) Connect(path string, handler core.HandlerFunc, mws ...core.Middleware) {
+	r.Handle(http.MethodConnect, path, handler, mws...)
+}
+
+func (r *Router) Head(path string, handler core.HandlerFunc, mws ...core.Middleware) {
+	r.Handle(http.MethodHead, path, handler, mws...)
+}
+
+func (r *Router) Options(path string, handler core.HandlerFunc, mws ...core.Middleware) {
+	r.Handle(http.MethodOptions, path, handler, mws...)
+}
+
+func (r *Router) Patch(path string, handler core.HandlerFunc, mws ...core.Middleware) {
+	r.Handle(http.MethodPatch, path, handler, mws...)
+}
+
+func (r *Router) Trace(path string, handler core.HandlerFunc, mws ...core.Middleware) {
+	r.Handle(http.MethodTrace, path, handler, mws...)
+}

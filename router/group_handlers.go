d9432b41035588089826b8d299b710d181d51c40
72 <72sevenzy2@gmail.com>
2026-08-12 15:39:32 +0500

    feat: add other method handlers.

--- router/group_handlers.go
+++ router/group_handlers.go
@@ -15,3 +15,31 @@ func (g *Group) Get(path string, handler core.HandlerFunc, mws ...core.Middlewar
 func (g *Group) Post(path string, handler core.HandlerFunc, mws ...core.Middleware) {
 	g.Handle(http.MethodPost, path, handler, mws...)
 }
+
+func (g *Group) Put(path string, handler core.HandlerFunc, mws ...core.Middleware) {
+	g.Handle(http.MethodPut, path, handler, mws...)
+}
+
+func (g *Group) Del(path string, handler core.HandlerFunc, mws ...core.Middleware) {
+	g.Handle(http.MethodDelete, path, handler, mws...)
+}
+
+func (g *Group) Trace(path string, handler core.HandlerFunc, mws ...core.Middleware) {
+	g.Handle(http.MethodTrace, path, handler, mws...)
+}
+
+func (g *Group) Connect(path string, handler core.HandlerFunc, mws ...core.Middleware) {
+	g.Handle(http.MethodConnect, path, handler, mws...)
+}
+
+func (g *Group) Head(path string, handler core.HandlerFunc, mws ...core.Middleware) {
+	g.Handle(http.MethodHead, path, handler, mws...)
+}
+
+func (g *Group) Patch(path string, handler core.HandlerFunc, mws ...core.Middleware) {
+	g.Handle(http.MethodPatch, path, handler, mws...)
+}
+
+func (g *Group) Options(path string, handler core.HandlerFunc, mws ...core.Middleware) {
+	g.Handle(http.MethodOptions, path, handler, mws...)
+}

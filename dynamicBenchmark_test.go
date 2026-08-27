6b8dc6fec12bef369ba0643f418822fb4c36de4e
72 <72sevenzy2@gmail.com>
2026-08-27 19:44:19 +0500

    test: add benchmarks for both dynamic/static route comparison between stdlib and xrouter.

---
+++ dynamicBenchmark_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/72sevenzy2/http-router/router"
+)
+
+func BenchmarkDynamicRoutes(t *testing.B) {
+	r := router.NewRouter()
+
+	r.Handle(http.MethodGet, "route/path-50/:id", func(w http.ResponseWriter, r *router.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	req := httptest.NewRequest(http.MethodGet, "/route/path-50/72", nil)
+	rr := httptest.NewRecorder()
+	t.ResetTimer()
+
+	for t.Loop() {
+		r.ServeHTTP(rr, req)
+	}
+}
+
+// comparing to go's stdlib mux
+func BenchmarkStdlibDynamic(t *testing.B) {
+	r := http.NewServeMux()
+
+	r.HandleFunc("route/path-50/:id", DummyHandler)
+
+	req := httptest.NewRequest(http.MethodGet, "/route/path-50/72", nil)
+	rr :=  httptest.NewRecorder()
+
+	t.ResetTimer()
+
+	for t.Loop() {
+		r.ServeHTTP(rr, req)
+	}
+}
+
+// shared with staticBenchmark_test.go
+func DummyHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
---
+++ staticBenchmark_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/72sevenzy2/http-router/router"
+)
+
+func BenchmarkStaticRoutes(t *testing.B) {
+	r := router.NewRouter()
+
+	r.Handle(http.MethodGet, "test/path/hi", func(w http.ResponseWriter, r *router.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	req := httptest.NewRequest(http.MethodGet, "/test/path/hi", nil)
+	rr := httptest.NewRecorder()
+
+	t.ResetTimer()
+
+	for t.Loop() {
+		r.ServeHTTP(rr, req)
+	}
+}
+
+func BenchmarkStdlibStatic(t *testing.B) {
+	r := http.NewServeMux()
+
+	r.HandleFunc("test/path/hi", DummyHandler)
+	req := httptest.NewRequest(http.MethodGet, "/test/path/hi", nil)
+	rr := httptest.NewRecorder()
+
+	t.ResetTimer()
+	for t.Loop() {
+		r.ServeHTTP(rr, req)
+	}
+}

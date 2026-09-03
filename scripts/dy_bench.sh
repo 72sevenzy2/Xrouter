7cd95490d5c70d6dc687076352827d72a3a67263
72 <72sevenzy2@gmail.com>
2026-09-03 15:30:37 +0500

    test: display memory profiling after benchmarks.

--- scripts/dy_bench.sh
+++ scripts/dy_bench.sh
@@ -2,5 +2,7 @@
 
 cd .. || exit 1
 cd benchmarks || exit 1
-go test -bench="^(BenchmarkXrouterDynamicRoutes|BenchmarkGolangStdlibRouterDynamicRoutes)$" -run=^$ -cpuprofile=1cpu.prof -memprofile=1mem.prof -count=10 > dynamicBenchmarks.txt
+go test -bench="^(BenchmarkXrouterDynamicRoutes|BenchmarkGolangStdlibRouterDynamicRoutes)$" -run=^$ -benchmem -cpuprofile=1cpu.prof -memprofile=1mem.prof -count=10 > dynamicBenchmarks.txt
 benchstat dynamicBenchmarks.txt
+
+go tool pprof 1mem.prof
--- scripts/st_bench.sh
+++ scripts/st_bench.sh
@@ -2,5 +2,7 @@
 
 cd .. || exit 1
 cd benchmarks || exit 1
-go test -bench="^(BenchmarkXrouterStaticRoutes|BenchmarkGolangStdlibStaticRoutes)$" -run=^$ -cpuprofile=2cpu.prof -memprofile=2mem.prof -count=10 > staticBenchmarks.txt
+go test -bench="^(BenchmarkXrouterStaticRoutes|BenchmarkGolangStdlibStaticRoutes)$" -run=^$ -benchmem -cpuprofile=2cpu.prof -memprofile=2mem.prof -count=10 > staticBenchmarks.txt
 benchstat staticBenchmarks.txt
+
+go tool pprof 2mem.prof
